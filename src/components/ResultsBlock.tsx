0a132ae15755fd2e5878abf1e1e2d07bc8bf56cb
galizaragoza <galizaragozam@gmail.com>
2025-04-06 00:17:00 +0200

    iconos nuevos

--- src/components/ResultsBlock.tsx
+++ src/components/ResultsBlock.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { FaRoad } from "react-icons/fa";
+import { FaPersonMilitaryRifle, FaUserDoctor, FaSchool } from "react-icons/fa6";
+import { GiAutoRepair, GiPoliceCar } from "react-icons/gi";
+import { MdFireTruck, MdOutlineVaccines } from "react-icons/md";
 
 export default function ResultsBlock({ resultados, tipo }) {
   return (
@@ -19,43 +23,43 @@ export default function ResultsBlock({ resultados, tipo }) {
         <div>
           <h4 className="text-lg font-semibold mb-4">💡 Con tu aporte se podría haber hecho...</h4>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-8">
-            <div className="border rounded-xl">🛣️ {(resultados.totalPagado / 350000).toFixed(3)} km de carretera asfaltada</div>
-            <div className="border rounded-xl">🛠️ {(resultados.totalPagado / 100000).toFixed(2)} km de carretera reparada</div>
-            <div className="border rounded-xl">🚓 {(resultados.totalPagado / 30000).toFixed(2)} sueldos anuales de policías</div>
-            <div className="border rounded-xl">👨‍🚒 {(resultados.totalPagado / 35000).toFixed(2)} sueldos anuales de bomberos</div>
-            <div className="border rounded-xl">🪖 {(resultados.totalPagado / 22000).toFixed(2)} sueldos anuales de militares</div>
-            <div className="border rounded-xl">🩺 {(resultados.totalPagado / 50000).toFixed(2)} sueldos anuales de médicos</div>
-            <div className="border rounded-xl">🏫 {(resultados.totalPagado / 6000).toFixed(2)} años de escolarización pública</div>
-            <div className="border rounded-xl">💉 {(resultados.totalPagado / 25).toFixed(0)} vacunas</div>
-            <div className="border rounded-xl">🎓 {(resultados.totalPagado / 2500).toFixed(0)} becas Erasmus</div>
-            <div className="border rounded-xl">🔬 {(resultados.totalPagado / 45000).toFixed(2)} becas doctorales FPI</div>
-            <div className="border rounded-xl">🛏️ {(resultados.totalPagado / 70000).toFixed(2)} camas UCI totalmente equipadas</div>
-            <div className="border rounded-xl">🚑 {(resultados.totalPagado / 90000).toFixed(2)} ambulancias medicalizadas</div>
-            <div className="border rounded-xl">🧠 {(resultados.totalPagado / 5000).toFixed(2)} talleres de salud mental en institutos</div>
-            <div className="border rounded-xl">🍼 {(resultados.totalPagado / 3500).toFixed(2)} plazas de guardería pública</div>
-            <div className="border rounded-xl">🌳 {(resultados.totalPagado / 20000).toFixed(2)} proyectos de reforestación local</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center"><FaRoad size={50}/> {(resultados.totalPagado / 350000).toFixed(3)} km de carretera asfaltada</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center"><GiAutoRepair size={50}/> {(resultados.totalPagado / 100000).toFixed(3)} km de carretera reparada</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center"><GiPoliceCar size={50}/> {(resultados.totalPagado / 30000).toFixed(1)} sueldos anuales de policías</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center"><MdFireTruck size={50}/> {(resultados.totalPagado / 35000).toFixed(1)} sueldos anuales de bomberos</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center"><FaPersonMilitaryRifle size={50}/> {(resultados.totalPagado / 22000).toFixed(1)} sueldos anuales de militares</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center"><FaUserDoctor size={50}/> {(resultados.totalPagado / 50000).toFixed(1)} sueldos anuales de médicos</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center"><FaSchool size={50}/> {(resultados.totalPagado / 6000).toFixed(1)} años de escolarización pública</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center"><MdOutlineVaccines size={50}/>{(resultados.totalPagado / 25).toFixed(1)} vacunas</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🎓 {(resultados.totalPagado / 2500).toFixed(1)} becas Erasmus</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🔬 {(resultados.totalPagado / 45000).toFixed(1)} becas doctorales FPI</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🛏️ {(resultados.totalPagado / 70000).toFixed(1)} camas UCI totalmente equipadas</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🚑 {(resultados.totalPagado / 90000).toFixed(1)} ambulancias medicalizadas</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🧠 {(resultados.totalPagado / 5000).toFixed(1)} talleres de salud mental en institutos</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🍼 {(resultados.totalPagado / 3500).toFixed(1)} plazas de guardería pública</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🌳 {(resultados.totalPagado / 20000).toFixed(1)} proyectos de reforestación local</div>
           </div>
         </div>
         <div>
           <h4 className="text-lg font-semibold mt-6 mb-4">🧢 Y también...</h4>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-8 border rounded-xl">
-            <div className="border rounded-xl">📱 {(resultados.totalPagado / 1300).toFixed(0)} iPhones 16</div>
-            <div className="border rounded-xl">👔 {(resultados.totalPagado / 82000).toFixed(2)} sueldos de ministros</div>
-            <div className="border rounded-xl">👑 {(resultados.totalPagado / 90000).toFixed(2)} sueldos del presidente del Gobierno</div>
-            <div className="border rounded-xl">🇪🇺 {(resultados.totalPagado / 108000).toFixed(2)} sueldos de eurodiputados</div>
-            <div className="border rounded-xl">👥 {(resultados.totalPagado / 55000).toFixed(2)} sueldos de asesores políticos</div>
-            <div className="border rounded-xl">🛫 {(resultados.totalPagado / 25000).toFixed(2)} vuelos del Falcon</div>
-            <div className="border rounded-xl">🖼️ {(resultados.totalPagado / 20000).toFixed(1)} obras de arte para edificios oficiales</div>
-            <div className="border rounded-xl">🕸️ {(resultados.totalPagado / 150000).toFixed(2)} webs institucionales de dudoso funcionamiento</div>
-            <div className="border rounded-xl">🎆 {(resultados.totalPagado / 5000).toFixed(0)} espectáculos de fuegos artificiales</div>
-            <div className="border rounded-xl">💾 {(resultados.totalPagado / 15).toFixed(0)} USBs con logos institucionales</div>
-            <div className="border rounded-xl">🧾 {(resultados.totalPagado / 1.5).toFixed(0)} folletos electorales impresos</div>
-            <div className="border rounded-xl">🗿 {(resultados.totalPagado / 35000).toFixed(2)} esculturas abstractas en rotondas</div>
-            <div className="border rounded-xl">🧴 {(resultados.totalPagado / 2500).toFixed(0)} caterings institucionales de lujo</div>
-            <div className="border rounded-xl">🪑 {(resultados.totalPagado / 6000).toFixed(1)} sillas de diseño para despachos públicos</div>
-            <div className="border rounded-xl">📸 {(resultados.totalPagado / 1000).toFixed(1)} sesiones de fotos oficiales con estilismo</div>
-            <div className="border rounded-xl">📦 {(resultados.totalPagado / 8).toFixed(0)} packs de merchandising electoral</div>
-            <div className="border rounded-xl">🧘 {(resultados.totalPagado / 3000).toFixed(1)} cursos de coaching político</div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-8">
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">📱 {(resultados.totalPagado / 1300).toFixed(0)} iPhones 16</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">👔 {(resultados.totalPagado / 82000).toFixed(2)} sueldos de ministros</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">👑 {(resultados.totalPagado / 90000).toFixed(2)} sueldos del presidente del Gobierno</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🇪🇺 {(resultados.totalPagado / 108000).toFixed(2)} sueldos de eurodiputados</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">👥 {(resultados.totalPagado / 55000).toFixed(2)} sueldos de asesores políticos</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🛫 {(resultados.totalPagado / 25000).toFixed(2)} vuelos del Falcon</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🖼️ {(resultados.totalPagado / 20000).toFixed(1)} obras de arte para edificios oficiales</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🕸️ {(resultados.totalPagado / 150000).toFixed(2)} webs institucionales de dudoso funcionamiento</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🎆 {(resultados.totalPagado / 5000).toFixed(0)} espectáculos de fuegos artificiales</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">💾 {(resultados.totalPagado / 15).toFixed(0)} USBs con logos institucionales</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🧾 {(resultados.totalPagado / 1.5).toFixed(0)} folletos electorales impresos</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🗿 {(resultados.totalPagado / 35000).toFixed(2)} esculturas abstractas en rotondas</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🧴 {(resultados.totalPagado / 2500).toFixed(0)} caterings institucionales de lujo</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🪑 {(resultados.totalPagado / 6000).toFixed(1)} sillas de diseño para despachos públicos</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">📸 {(resultados.totalPagado / 1000).toFixed(1)} sesiones de fotos oficiales con estilismo</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">📦 {(resultados.totalPagado / 8).toFixed(0)} packs de merchandising electoral</div>
+            <div className="border rounded-xl p-4 shadow-lg flex flex-col gap-4 items-center">🧘 {(resultados.totalPagado / 3000).toFixed(1)} cursos de coaching político</div>
           </div>
         </div>
 
